b5d101a7e909f0786d726de09711f5e11c8abbe0
gandomullac <gandomullac@gmail.com>
2024-02-01 17:40:06 +0100

    Correzione broken link

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="fixed-top d-flex align-items-cente">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
 
-      <a href="index.html" class="logo me-auto me-lg-0"><img src={{ asset('assets/img/logo.png') }} alt="" class="img-fluid"></a>
+      <a href="{{ route('homepage') }}" class="logo me-auto me-lg-0"><img src={{ asset('assets/img/logo.png') }} alt="" class="img-fluid"></a>
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
@@ -9,8 +9,12 @@
           <li><a class="nav-link scrollto" href="#about">Chi siamo</a></li>
           <li><a class="nav-link scrollto" href="#gallery">Galleria</a></li>
           <li><a class="nav-link scrollto" href="#specials">Attività</a></li>
-          <li><a class="nav-link scrollto" href="#events">Eventi</a></li>
-          <li><a class="nav-link scrollto" href="#menu">Menù </a></li>
+          @if($events->count() > 0)
+            <li><a class="nav-link scrollto" href="#events">Eventi</a></li>
+          @endif
+          @if($menuItems->count() > 0)
+            <li><a class="nav-link scrollto" href="#menu">Menù </a></li>
+          @endif
 
           <li><a class="nav-link scrollto" href="#contact">Contatti</a></li>
         </ul>
