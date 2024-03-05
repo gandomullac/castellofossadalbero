45d8f0a65dd84b056d20d13cfef9d9b5f901dce1
gandomullac <gandomullac@gmail.com>
2024-03-05 11:34:25 +0100

    Fix avatar reviews

--- resources/views/main/reviews/item.blade.php
+++ resources/views/main/reviews/item.blade.php
@@ -5,7 +5,7 @@
             {{ $item->content }}
             <i class="bx bxs-quote-alt-right quote-icon-right"></i>
         </p>
-        <img src="{{ asset('assets/img/avatar-person.svg') }}" class="testimonial-img" alt="">
+        <img src="{{ asset('assets/img/placeholders/avatar-person.svg') }}" class="testimonial-img" alt="">
         <h3>{{ $item->author }}</h3>
         <h4>
             <a href="{{ $item->url }}" target="_blank">
