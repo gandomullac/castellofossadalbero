16fbda99293700dd888c78e5c413551a455c5993
gandomullac <gandomullac@gmail.com>
2024-07-11 11:39:36 +0200

    Correzione errore orario piscina

--- resources/views/main/swimming-pool.blade.php
+++ resources/views/main/swimming-pool.blade.php
@@ -1,87 +1,89 @@
 <section id="piscina" class="specials">
     <div class="container" data-aos="fade-up">
 
-      <div class="section-title">
-        <h2>Attività</h2>
-        <p>Stagione 2024 in Piscina</p>
-      </div>
+        <div class="section-title">
+            <h2>Attività</h2>
+            <p>Stagione 2024 in Piscina</p>
+        </div>
 
-      <div class="row">
-        <div class="col-md-7">
-          <p class="mb-4">
-            Anche quest'anno con l'arrivo della bella stagione la piscina del Castello di Fossadalbero si apre al pubblico.
-          </p>
-          <p>
-            Gli orari della piscina sono dalle <strong>9:00 alle 19:00, dal lunedì alla domenica</strong>.
-          </p>
-            <h6>Abbonamenti</h6>
-          <ul>
-            <li>
-              Abbonamento stagionale piscina (1 giugno / 15 settembre) - €  350,00
-            </li>
-            <li>
-              Abbonamento mensile (validità 30 giorni dalla emissione) - € 120,00
-            </li>
-            <li>
-              Abbondamento bambini dai 6 a 12 anni - € 200,00
-            </li>
-            <li>
-              Abbondamento bambini mensile - € 80,00
-            </li>
-          </ul>
+        <div class="row">
+            <div class="col-md-7">
+                <p class="mb-4">
+                    Anche quest'anno con l'arrivo della bella stagione la piscina del Castello di Fossadalbero si apre
+                    al pubblico.
+                </p>
+                <p>
+                    Gli orari della piscina sono dalle <strong>9:00 alle 19:00, dal lunedì alla domenica</strong>.
+                </p>
+                <h6>Abbonamenti</h6>
+                <ul>
+                    <li>
+                        Abbonamento stagionale piscina (1 giugno / 15 settembre) - € 350,00
+                    </li>
+                    <li>
+                        Abbonamento mensile (validità 30 giorni dalla emissione) - € 120,00
+                    </li>
+                    <li>
+                        Abbondamento bambini dai 6 a 12 anni - € 200,00
+                    </li>
+                    <li>
+                        Abbondamento bambini mensile - € 80,00
+                    </li>
+                </ul>
 
-          <h6 class="mt-4">Entrate giornaliere</h6>
-          <ul>
-            <li>
-              Ingresso giornaliero nelle giornate feriali - € 10,00
-            </li>
-            <li>
-              Ingresso giornaliero weekend e festivi - € 20,00
-            </li>
-            <li>
-              Ingresso mezza giornata festivo - € 10,00
-            </li>
-            <li>
-              Ingresso mezza giornata feriale - € 6,00
-            </li>
-          </ul>
-          <ul>
-            <li>
-              Persone con disabilità - Gratuito
-            </li>
-            <li>
-              Bambini sotto i 6 anni - Gratuito
-            </li>
-            <li>
-              Bambini da 6 a 14 anni - € 5,00
-            </li>
-          </ul>
+                <h6 class="mt-4">Entrate giornaliere</h6>
+                <ul>
+                    <li>
+                        Ingresso giornaliero nelle giornate feriali - € 10,00
+                    </li>
+                    <li>
+                        Ingresso giornaliero weekend e festivi - € 20,00
+                    </li>
+                    <li>
+                        Ingresso mezza giornata festivo - € 10,00
+                    </li>
+                    <li>
+                        Ingresso mezza giornata feriale - € 6,00
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Persone con disabilità - Gratuito
+                    </li>
+                    <li>
+                        Bambini sotto i 6 anni - Gratuito
+                    </li>
+                    <li>
+                        Bambini da 6 a 12 anni - € 5,00
+                    </li>
+                </ul>
 
-          <h6 class="mt-4">Servizi inclusi</h6>
-          <ul>
-            <li>
-              Lettini e ombrelloni.
-            </li>
-          </ul>
+                <h6 class="mt-4">Servizi inclusi</h6>
+                <ul>
+                    <li>
+                        Lettini e ombrelloni.
+                    </li>
+                </ul>
 
-          <h6 class="mt-4">Sport</h6>
-          <p>Fanno parte della struttura sportiva campi da basket, tennis, calcetto, beach volley.</p>
+                <h6 class="mt-4">Sport</h6>
+                <p>Fanno parte della struttura sportiva campi da basket, tennis, calcetto, beach volley.</p>
 
-          <h6 class="mt-4">Ristorazione</h6>
-          <ul>
-            <li>Chiringuito bar per apertivi e pranzi a bordo piscina</li>
-            <li>Ristorante all'interno del Castello e nel parco secolare</li>
-          </ul>
+                <h6 class="mt-4">Ristorazione</h6>
+                <ul>
+                    <li>Chiringuito bar per apertivi e pranzi a bordo piscina</li>
+                    <li>Ristorante all'interno del Castello e nel parco secolare</li>
+                </ul>
 
-          <p>
-            <a href="{{ asset('assets/pdf/brochure_piscina.pdf') }}" download> Clicca qui per
-              scaricare la brochure completa (pdf)</a>.
-          </p>
+                <p>
+                    <a href="{{ asset('assets/pdf/brochure_piscina.pdf') }}" download> Clicca qui per
+                        scaricare la brochure completa (pdf)</a>.
+                </p>
+
+            </div>
+            <div class="col-md-5">
+                <img src="{{ asset('assets/img/aperitivo-piscina.jpg') }}"
+                    alt="Foto piscina al tramonto durante un aperitivo" class="img-fluid">
+            </div>
 
         </div>
-        <div class="col-md-5">
-          <img src="{{ asset('assets/img/aperitivo-piscina.jpg') }}" alt="Foto piscina al tramonto durante un aperitivo" class="img-fluid">
-      </div>
-
-    </div>
-  </section>
+</section>
